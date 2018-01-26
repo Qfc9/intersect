abade5d18fea12d139d573607b97980daa01461a
Elijah Harmon <elijahharmon@gmail.com>
2018-01-26 10:31:13 -0500

    Added basic word value calculation function

--- util.c
+++ util.c
@@ -6,17 +6,23 @@
 
 size_t wordValue(char *word)
 {
+    size_t value = 0;
 
-  return 0;
+    for (size_t i = 0; i < strlen(word); i++)
+    {
+        value += word[0];
+    }
+
+    return value;
 }
 
 // Removes extra chars at the end of stdin
 void
 getCharClean (void)
 {
-  int clearChar = getchar ();
-  while ((clearChar != '\n') && (clearChar != EOF))
+    int clearChar = getchar ();
+    while ((clearChar != '\n') && (clearChar != EOF))
     {
-      clearChar = getchar ();
+        clearChar = getchar ();
     }
 }
