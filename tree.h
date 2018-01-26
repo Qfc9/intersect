cc1883ccf9fe5c6c208f3d734df90609cb8a8354
Elijah Harmon <elijahharmon@gmail.com>
2018-01-25 21:57:41 -0500

    Added my file reading lib

---
+++ tree.h
@@ -0,0 +1,15 @@
+#ifndef tree_H
+#define tree_H
+
+#include <stdlib.h>
+
+typedef struct _tree tree;
+tree *createTree (void);
+void processCompany (tree ** t, char *line);
+size_t treeHeight (tree * a);
+void treeUpdate (tree ** t, char *ticker, char *name, double value);
+void treeInsert (tree ** a, char *symbol, char *name, size_t price);
+void treePrint (const tree * a);
+void treeDisassemble (tree * a);
+
+#endif
